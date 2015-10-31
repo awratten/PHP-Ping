339e5922a0e3defdc0c71cfb26f4d3a5e8f9a38f
awratten <awratten1@gmail.com>
2015-10-31 10:47:22 +1000

    Update Ping.php
    added the count flag for ping

--- Ping.php
+++ Ping.php
@@ -4,18 +4,17 @@
 body
 {
 font-family:Consolas,Menlo,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mo$
-
 }
 </style>
 <body>
 
 <?php
-    $host = $_GET['host'];
-    exec("ping -c 1 ".escapeshellarg($host), $output, $status);
-    echo implode('<br>',$output);
+$host = $_GET['host'];
+$c = $_GET['c']?: '1';
+
+exec("ping -c $c ".escapeshellarg($host), $output, $status);
+echo implode('<br>',$output);
 ?>
-    </body>
+
+</body>
 </html>
-
-
-
